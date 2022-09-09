1b2cd5f3ba1b59c1d6e26cb22bd007293ed82d46
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-09 10:56:38 +0530

    modified:max strings

---
+++ src/FindMaximumString.java
@@ -0,0 +1,22 @@
+public class FindMaximumString {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Problem using Generics");
+        String e = "Apple", f = "Orange" , g = "Mango";    //take variables strings
+
+        //calling directly without creating object bcoz testMaximum method also static.
+        testMaximum (e,f,g);//calling method
+    }
+    //creating testMaximum method and passing parameter in string type
+    private static void testMaximum(String e, String f, String g) {
+        String max = e; //checking condition comparing and finding max string
+        if(f.compareTo(e)>0){
+            max = f;
+        }
+        if(g.compareTo(max)>0){
+            max = g;
+        }
+        System.out.println("Maximum String is: "+max);//print maximum string
+
+    }
+
+}
