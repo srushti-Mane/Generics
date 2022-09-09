02a6a99e8f1280d47dd669910d71a3c179662fb9
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-09 10:48:27 +0530

    modified:max floats

---
+++ src/FindMaximumFloat.java
@@ -0,0 +1,20 @@
+public class FindMaximumFloat {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Float Problem using Generics");
+        Float a= 29.89f, b = 9.99f, c = 9.96f;  //takiing variables float no.
+
+        testMaximum(a, b, c);  //calling method
+    }
+    //creating testMaximum method and passing parameters.
+    private static void testMaximum(Float a, Float b, Float c) {
+        Float max = a; //checking condition and find max float no
+        if (b.compareTo(a) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println("Maximum Number is: " + max); //print maximum float no.
+
+    }
+}
