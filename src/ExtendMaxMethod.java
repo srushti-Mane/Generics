c4e23438052e34449f75e0dde5ccf15e8e2f0362
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-09 11:05:28 +0530

    modified:extend max method

---
+++ src/ExtendMaxMethod.java
@@ -0,0 +1,37 @@
+
+    // create generic class
+    public class ExtendMaxMethod<T extends Comparable<T>> {
+
+        // create genterics maxmumValue method and passing parameters
+
+        public static <T extends Comparable<T>> T maximumValue(T val1, T val2, T val3, T val4, T val5) {
+
+            T max = val1;
+            //checking conditions
+            if (val2.compareTo(max) > 0) {
+                max = val2;
+            }
+            if (val3.compareTo(max) > 0) {
+                max = val3;
+            }
+            if (val4.compareTo(max) > 0) {
+                max = val4;
+            }
+            if (val5.compareTo(max) > 0) {
+                max = val5;
+
+            }
+            return max;
+        }
+
+        public static void main(String[] args) {
+
+            FindMaximum max = new FindMaximum(); // creating object for FindMaximum class object name is max
+
+            //print maximum value
+            System.out.println("The maximum value is : " + maximumValue(29.89f, 9.99f, 9.96f, 15.94f, 8.25f));
+
+
+        }
+    }
+
